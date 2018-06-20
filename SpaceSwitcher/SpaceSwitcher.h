eff2bd1c5744a220b2aa6d2fa4ee0d3026499fe7
Andy Park <andy@bigbearlabs.com>
2018-06-20 13:58:15 +0200

    Initial Commit

---
+++ SpaceSwitcher/SpaceSwitcher.h
@@ -0,0 +1,19 @@
+//
+//  SpaceSwitcher.h
+//  SpaceSwitcher
+//
+//  Created by ilo on 20/06/2018.
+//  Copyright © 2018 Big Bear Labs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SpaceSwitcher.
+FOUNDATION_EXPORT double SpaceSwitcherVersionNumber;
+
+//! Project version string for SpaceSwitcher.
+FOUNDATION_EXPORT const unsigned char SpaceSwitcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpaceSwitcher/PublicHeader.h>
+
+
